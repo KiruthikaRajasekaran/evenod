c8fbfeb5bd79df55f26b583dfaf72bc4f6c06349
KiruthikaRajasekaran <krithikasadhana213@gmail.com>
2016-07-09 11:46:39 +0530

    finish

---
+++ README.c
@@ -0,0 +1,18 @@
+# evenod
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int a;
+printf("enter the number");
+if(a%2==0)
+{
+printf("a is even");
+}
+else
+{
+printf("a is odd");
+}
+getch();
+}
